65d58c874e381a2c1f3cfd83f782e9b1a8b89a18
GPXPWRUSR <suyash.dhondkar@globalpayex.co.in>
2022-06-07 15:13:11 +0530

    Shorter for loop

--- src/CreateStudent.java
+++ src/CreateStudent.java
@@ -4,24 +4,24 @@ public class CreateStudent
 	
 	public static void main(String[] args)
 	{
-		//Student [] StudentList = new Student[2]; 
-		Student s1 = Student.newStudent("Suyash",'M',35,77.6f);
-		Student s2 = Student.newStudent("Dhondkar",'F',25,25.6f);
-		for(int i = 0; i < 2;i++)
+		Student [] StudentList = new Student[2]; 
+		StudentList[0] = Student.newStudent("Suyash",'M',35,77.6f);
+		StudentList[1] = Student.newStudent("Dhondkar",'F',25,25.6f);
+		for(Student student : StudentList)
 		{
-			//System.out.println(StudentList[i].name+ " "+ StudentList[i].gender);
+			System.out.println(student.name);
 		}
 		//Student s2 = new Student();
 		//Student s3 = new Student();
-		System.out.println(s1);
+		//System.out.println(s1);
 		//System.out.println(s2);
 		/*
 		s1.name = "Suyash";
 		s1.rollno = 35;
 		s1.gender = 'M';
 		s1.marks = 77.36f;*/
-		System.out.println(Student.count);
+		/*System.out.println(Student.count);
 		System.out.println(s1.getDetails());
-		System.out.println(s1.getGrade());
+		System.out.println(s1.getGrade());*/
 	}
 }
